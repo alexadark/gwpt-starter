22dae964bd47fa0441a2efe6963d32ee28174cac
Alexandra Spalato <alexaspalato@gmail.com>
2021-03-15 08:30:25 +0100

    Revert "Merge branch 'refs/heads/main' into dev"
    This reverts commit a1df433ddea2aa281127beccdc2d332777c9f3e0, reversing
    changes made to 6accb2097123ed81578e8354e7fcf08d0fb6673d.

--- src/@gatsbywpthemes/gatsby-theme-wp-starter/components/Layout.js
+++ src/@gatsbywpthemes/gatsby-theme-wp-starter/components/Layout.js
@@ -1,8 +1,5 @@
 import React from "react"
 import { Layout as StyledLayout } from "starterComponents/Layout"
-import { HeadlessProvider } from '@wpengine/headless/react';
 import "../../../styles/style.scss"
 
-export const Layout = (props) => <HeadlessProvider pageProps={props} {...props} >
-<StyledLayout />
-</HeadlessProvider>
+export const Layout = (props) => <StyledLayout {...props} />
